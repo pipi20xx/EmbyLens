377bbcb837213944df13f3af83eadd2ae07d6261
admin <admin@123.com>
2026-01-25 21:32:03 +0800

    2.1.8

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -295,8 +295,8 @@ const message = useMessage()
 const upgrading = ref(false)
 
 const versionInfo = ref({
-        current: 'v2.1.7',
-        latest: 'v2.1.7',  hasUpdate: false
+        current: 'v2.1.8',
+        latest: 'v2.1.8',  hasUpdate: false
 })
 
 const handleUpgrade = async () => {
